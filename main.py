c4945b1472a291dcb2b3329a2b7f796b34498992
cuberbug <gigobug@gmail.com>
2024-02-12 15:50:39 +0300

    fix pip8

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import argparse
-import sqlite3
 import os.path
+import sqlite3
 from pathlib import Path
+
 from pytube import YouTube
-  
-    
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Vimusic-downloader: download all favorite tracks from ViMusic database (webm/opus format)')
+    parser = argparse.ArgumentParser(
+        description=(
+            "Vimusic-downloader: download all favorite tracks"
+            "from ViMusic database (webm/opus format)"
+        )
+    )
     parser.add_argument('database', type=str, help='database file')
     args = parser.parse_args()
 
-    if os.path.isfile(args.database) != True:
+    if not os.path.isfile(args.database):
         print("File is not exist!")
         exit(1)
 
@@ -23,38 +28,45 @@ def main():
     tracks = cursor.fetchall()
 
     cursor.execute("SELECT COUNT(likedAt) FROM Song")
-    tracks_count = 0 
-    tracks_count1 = cursor.fetchall() # костыляем
+    tracks_count = 0
+    tracks_count1 = cursor.fetchall()  # костыляем
 
-    for counts in tracks_count1: # лютий костыль, потому что sqlite3.Row возвращает адрес в памяти, если к нему обращаться напрямую
+    # лютий костыль, потому что sqlite3.Row возвращает адрес в памяти,
+    # если к нему обращаться напрямую
+    for counts in tracks_count1:
         tracks_count = counts[0]
 
     musicdir = str(Path.home()) + "/Music/"
-    if os.path.exists(musicdir) != True:
-            os.mkdir(musicdir)
+    if not os.path.exists(musicdir):
+        os.mkdir(musicdir)
 
-    print("Favorites: " + str(tracks_count) + " tracks")
-  
+    print(f"Favorites: {tracks_count} tracks")
+
     for count, track in enumerate(tracks):
         print("\nFetching info...")
-      
+
         obj = YouTube.from_id(track[0])
-        name =  f"{obj.title} - {obj.author}"
-        filename = f"{name}.webm" # todo: выбор формата webm/m4a
+        name = f"{obj.title} - {obj.author}"
+        filename = f"{name}.webm"  # todo: выбор формата webm/m4a
         stream = obj.streams.get_by_itag(251)
         size = str(stream.filesize_mb)
         size_bytes = stream.filesize
-      
+
         print(f"Downloading track: {name} ({count + 1} of {tracks_count})")
-        print(f"Size: {str(size)} MB")
-      
-        if os.path.isfile(musicdir + filename) == True and os.path.getsize(musicdir + filename) == size_bytes: # проверка на то что файл не битый и существует, иначе пропускаем
+        print(f"Size: {size} MB")
+
+        # проверка на то что файл не битый и существует, иначе пропускаем
+        if (
+            os.path.isfile(musicdir + filename)
+            and os.path.getsize(musicdir + filename) == size_bytes
+        ):
             print("File exist, skipping...")
             continue
-          
+
         stream.download(filename=filename, output_path=musicdir)
 
     db.close()
+
 
 if __name__ == "__main__":
     main()
